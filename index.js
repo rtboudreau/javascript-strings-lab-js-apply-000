10e07d608e6d764c99da24492c63077aa3fe1e09
Reid Boudreau <rtboudreau@gmail.com>
2018-12-25 14:06:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// define greeting
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+
+// concatenate string to greet special guest
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+// interpolate string 
 var conversation = `Let's talk about ${topic}.`;
